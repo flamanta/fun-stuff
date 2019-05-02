5e90ffda6a8a077ecd888a2068e22f3bc5c98f2d
flamanta <48215474+flamanta@users.noreply.github.com>
2019-05-03 00:54:07 +0800

    + Imperial units, Catch wrong input type

--- BMI_calc.py
+++ BMI_calc.py
@@ -1,9 +1,25 @@
-weight = float(input("Weight in kg? "))
-height = float(input("Height in cm? "))
-
-def bmi_calc(weight, height):
-    height = height/100
-    bmi = round(weight/(height** 2), 1)
+def bmi_calc(system = 'metric'):
+    if system == 'metric':
+        try:
+            kg = float(input("Weight in kg? "))
+        except ValueError:
+            return 'Wrong input type for weight in kg. Please input a float.'
+        try:
+            cm = float(input("Height in cm? "))
+        except ValueError:
+            return 'Wrong input type for height in cm. Please input a float.'
+        m = cm/100
+        bmi = round(kg/(m** 2), 1)
+    elif system == 'imperial':
+        try:
+            lb = float(input("Weight in lb? "))
+        except ValueError:
+            return 'Wrong input for weight in lb. Please input a float.'
+        try:
+            inch = float(input("Height in inches? "))
+        except ValueError:
+            return 'Wrong input for height in inches. Please input a float.'
+        bmi = round(lb/(inch)**2*703, 1)    
     if bmi >= 27.5:
         return "Your BMI is {}, and it is in the high risk range.".format(bmi)
     elif bmi >= 23:
@@ -13,7 +29,6 @@ def bmi_calc(weight, height):
     else:
         return "Your BMI is {}, and you are at risk of nutritional deficiency diseases and osteoporosis.".format(bmi)
 
-print(bmi_calc(weight, height))
+print(bmi_calc())
 
 # To be rewritten as a class
-# In metric and imperial units
